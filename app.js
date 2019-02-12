16149e707a8b6bb6a9871f7827b056b8449f523b
Carlos Costa <carlosdaviddacosta@gmail.com>
2019-02-12 15:30:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.getElementById('button').addEventListener('click',getJoke);
 
 function getJoke(){
   const xhr = new XMLHttpRequest();
-  xhr.open('GET','https://api.icndb.com/jokes/random',true);
+  xhr.open('GET','http://api.icndb.com/jokes/random',true);
   xhr.onload = function() {
     if(this.status === 200){
       let response = JSON.parse(this.responseText);
